41915386c0ef5c539fb2e0e2c7a741e683d02b87
Dmitry B <render1980@gmail.com>
2018-09-28 11:18:31 +0300

    Create php-custom.el

---
+++ roles/mac/config/.emacs.d/custom/php-custom.el
@@ -0,0 +1,29 @@
+{;;; package --- php-mode custom config
+;;; Commentary:
+;;;
+;;; Code:
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html\\.twig\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+				   
+;; FLYCHECK ;;
+(require 'flymake)
+
+(defun flymake-php-init ()
+  "Use php to check the syntax of the current file."
+  (let* ((temp (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))
+	 (local (file-relative-name temp (file-name-directory buffer-file-name))))
+    (list "php" (list "-f" local "-l"))))
+
+(add-to-list 'flymake-err-line-patterns
+  '("\\(Parse\\|Fatal\\) error: +\\(.*?\\) in \\(.*?\\) on line \\([0-9]+\\)$" 3 4 nil 2))
+
+(add-to-list 'flymake-allowed-file-name-masks '("\\.php$" flymake-php-init))
+[]
+(add-hook 'php-mode-hook (lambda () (flymake-mode 1)))
+;; (define-key php-mode-map '[M-S-up] 'flymake-goto-prev-error)
+;; (define-key php-mode-map '[M-S-down] 'flymake-goto-next-error)
+
+(provide 'php-custom)
+;;; php-custom.el ends here
