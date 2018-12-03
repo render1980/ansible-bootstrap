d20727646bc8d0881ae2dee4d8215e5b9a8f624c
Dmitry B <render1980@gmail.com>
2018-12-03 14:02:14 +0300

    Update js-custom.el

--- roles/mac/config/.emacs.d/custom/js-custom.el
+++ roles/mac/config/.emacs.d/custom/js-custom.el
@@ -15,6 +15,7 @@
 (add-hook 'js2-mode-hook #'js2-refactor-mode)
 (js2r-add-keybindings-with-prefix "C-c C-r")
 (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+(define-key js2-mode-map (kbd "s-b") #'js2-jump-to-definition)
 
 ;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
 ;; unbind it.
