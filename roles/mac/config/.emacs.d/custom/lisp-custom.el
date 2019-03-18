393befdbbe3375553c04219fff88391c635bf518
Dmitry B <render1980@gmail.com>
2019-03-18 21:05:30 +0300

    Update lisp-custom.el

--- roles/mac/config/.emacs.d/custom/lisp-custom.el
+++ roles/mac/config/.emacs.d/custom/lisp-custom.el
@@ -11,10 +11,6 @@
                 ;; Pretty-print eval'd expressions.
                 (define-key emacs-lisp-mode-map
                             "\C-x\C-e" 'pp-eval-last-sexp)
-                ;; Recompile if .elc exists.
-                (add-hook (make-local-variable 'after-save-hook)
-                          (lambda ()
-                            (byte-force-recompile default-directory)))
                 (define-key emacs-lisp-mode-map
                             "\r" 'reindent-then-newline-and-indent)))
     (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
